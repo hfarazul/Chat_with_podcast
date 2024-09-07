98d79dda18d810dfa453d176e1f0cd9498f205a7
hfarazul <h.farazul@gmail.com>
2024-09-07 18:55:06 +0530

    vercel changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 import logging
 from flask import Flask, request, jsonify, send_from_directory, send_file
+from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import openai
@@ -14,11 +15,13 @@ load_dotenv()
 
 app = Flask(__name__)
 
+CORS(app)
+
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
 
 # Set your OpenAI API key
-openai.api_key = os.getenv('YOUR_OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/hello-world')
 def hello_world():
