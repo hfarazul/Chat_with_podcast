7a47e467f56c4dbcd20f126ee92b696d90c25e06
hfarazul <h.farazul@gmail.com>
2024-09-10 23:12:47 +0530

    added maximise.ai

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://spaces-us-1.nyc3.digitaloceanspaces.com/RMieIJj0M7RIDmr7/AUcWfqBs-CO-IgP2qUT7n/pixel.js"
+          strategy="afterInteractive"
+          defer
+          async
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
